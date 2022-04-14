b852bf15a21f5158a730b6343c2e313f7ae4125d
Jiwoongyoo1 <dbwldnd89@gamil.com>
2022-04-14 22:02:11 +0900

    코드정리

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const renders = require('./renders')
 const router = require('./routers')
 const cors = require('cors')
 const app = express()
-const fs = require('fs'); // 파일시스템
 
 const socketIo = require('socket.io')
 const server = require('http').createServer(app)
@@ -20,20 +19,13 @@ const io = socketIo(server, {
 // 모든 도메인 허용 
 app.use(cors());
 
-app.use(express.static('uploadedFiles'))
 app.use(express.urlencoded({extended: false}), router)
 app.use(express.json())
 
-app.set('views', __dirname + '/views')
-app.set('view engine', 'ejs')
-
 const connect = require('./schemas');
-const { Iot } = require('aws-sdk');
 connect()
 
-
 app.use('/api', router)
-app.use('/', renders)  
 
 server.listen(3000, () => {
     console.log('3000번 서버가 정상적으로 켜졌습니다')
--- renders/detail_map.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.get('/', (req, res)=> {
-    res.render('detail_map')
-})
-
-module.exports = router
